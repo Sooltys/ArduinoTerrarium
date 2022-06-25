7b0ff1435f3673dabd1b7aea3e97cd06165fa41f
Sooltys <78873970+Sooltys@users.noreply.github.com>
2022-06-25 12:15:07 +0200

    delete...

--- app/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-d:\ArduinoTerrarium\build\libraries\Wire\utility\twi.c.o: \
- C:\Users\Kuba\Desktop\arduino-1.8.10\hardware\arduino\avr\libraries\Wire\src\utility\twi.c \
- C:\Users\Kuba\Desktop\arduino-1.8.10\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Users\Kuba\Desktop\arduino-1.8.10\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Users\Kuba\Desktop\arduino-1.8.10\hardware\arduino\avr\variants\standard/pins_arduino.h \
- C:\Users\Kuba\Desktop\arduino-1.8.10\hardware\arduino\avr\libraries\Wire\src\utility\twi.h
